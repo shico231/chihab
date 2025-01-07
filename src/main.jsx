c44ecc222bf1c65ae1da1e9eade72f2045fc761a
Chakib <shico231@gmail.com>
2025-01-07 16:46:25 +0100

    above

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import 'tachyons/css/tachyons.min.css';
-
-createRoot(document.getElementById('root')).render(
-  <div>
-  <App />
-  </div>
-)
